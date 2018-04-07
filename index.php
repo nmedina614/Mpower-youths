80cdb22b3c3e86ecb70dca9974df529695455cdb
Aaron Melhaff <booglessmith@gmail.com>
2018-04-07 02:47:02 -0400

    Added base files

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * TODO
+ * @author Aaron Melhaff <amelhaff2@mail.greenriver.edu>
+ * @version 1.0
+ */
+
+session_start();
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+// Require f3
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+Model::connect();
+
+$f3->route('GET /', function($f3) {
+
+    // Check login status
+    if(isset($_SESSION['username'])) {
+        // Title to use in template.
+        $title = "M-Power Youth";
+
+        // List of paths to stylesheets.
+        $styles = array(
+            //$f3->get('BASE').'/styles/main.css'
+        );
+
+        // List of paths for sub-templates being used.
+        $includes = array(
+            //'views/_nav.html',
+        );
+
+        // List of paths to scripts being used.
+        $scripts = array(
+        );
+
+        $f3->set('title' , $title);
+        $f3->set('styles' , $styles);
+        $f3->set('includes' , $includes);
+        $f3->set('scripts' , $scripts);
+
+        $template = new Template();
+        echo $template->render('views/_base.html');
+
+    } else {
+
+        $f3->reroute('login');
+
+    }
+});
+
+$f3->run();
