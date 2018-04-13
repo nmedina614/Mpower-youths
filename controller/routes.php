2900417ead2a78d32efc3d410a9f0c092f05a63d
Aaron Melhaff <booglessmith@gmail.com>
2018-04-13 17:27:10 -0400

    Moved routes to routes.php

--- controller/routes.php
+++ controller/routes.php
@@ -5,3 +5,90 @@
  * Date: 4/13/18
  * Time: 12:57 PM
  */
+
+$f3->route('GET /', function($f3) {
+
+    // Title to use in template.
+    $title = "M-Power Youth";
+
+    // List of paths to stylesheets.
+    $styles = array(
+        'assets/styles/_home.css'
+    );
+
+    // List of paths for sub-templates being used.
+    $includes = array(
+        'views/_nav.html',
+        'views/_home.html'
+    );
+
+    // List of paths to scripts being used.
+    $scripts = array(
+    );
+
+    $f3->set('title' , $title);
+    $f3->set('styles' , $styles);
+    $f3->set('includes' , $includes);
+    $f3->set('scripts' , $scripts);
+
+    $template = new Template();
+    echo $template->render('views/_base.html');
+});
+
+$f3->route('GET /gallery', function($f3) {
+
+    // Title to use in template.
+    $title = "M-Power Gallery";
+
+    // List of paths to stylesheets.
+    $styles = array(
+        //$f3->get('BASE').'/styles/main.css'
+    );
+
+    // List of paths for sub-templates being used.
+    $includes = array(
+        'views/_nav.html',
+        'views/_gallery.html'
+    );
+
+    // List of paths to scripts being used.
+    $scripts = array(
+    );
+
+    $f3->set('title' , $title);
+    $f3->set('styles' , $styles);
+    $f3->set('includes' , $includes);
+    $f3->set('scripts' , $scripts);
+
+    $template = new Template();
+    echo $template->render('views/_base.html');
+});
+
+$f3->route('GET /account', function($f3) {
+
+    // Title to use in template.
+    $title = "Account Management";
+
+    // List of paths to stylesheets.
+    $styles = array(
+        'assets/styles/_home.css'
+    );
+
+    // List of paths for sub-templates being used.
+    $includes = array(
+        'views/_nav.html',
+        'views/_account.html'
+    );
+
+    // List of paths to scripts being used.
+    $scripts = array(
+    );
+
+    $f3->set('title' , $title);
+    $f3->set('styles' , $styles);
+    $f3->set('includes' , $includes);
+    $f3->set('scripts' , $scripts);
+
+    $template = new Template();
+    echo $template->render('views/_base.html');
+});
