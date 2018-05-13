0532fa00e741fe8851f0bea0a8ef083849c67790
Nolan Medina <njmedina614@gmail.com>
2018-05-13 02:13:39 -0400

    added testimonial page and route

--- controller/routes.php
+++ controller/routes.php
@@ -504,3 +504,23 @@ $f3->route('GET /videos', function($f3) {
     $template = new Template();
     echo $template->render('views/_base.html');
 });
+$f3->route('GET /testimonials', function($f3) {
+    // Title to use in template.
+    $title = "M-Power Youth: Testimonials";
+    // List of paths to stylesheets.
+    $styles = array();
+    // List of paths for sub-templates being used.
+    $includes = array(
+        'views/_nav.html',
+        'views/_testimonials.html',
+        'views/_footer.html'
+    );
+    // List of paths to scripts being used.
+    $scripts = array();
+    $f3->set('title' , $title);
+    $f3->set('styles' , $styles);
+    $f3->set('includes' , $includes);
+    $f3->set('scripts' , $scripts);
+    $template = new Template();
+    echo $template->render('views/_base.html');
+});
